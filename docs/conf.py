e7ba60a1bd9fcc30614b539ea6a09c4410124437
karnwatcharasupat <karn001@e.ntu.edu.sg>
2021-11-02 23:14:34 -0400

    hide privates

--- docs/conf.py
+++ docs/conf.py
@@ -30,6 +30,7 @@ author = 'Karn N. Watcharasupat and Alexander Lerch'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon', 'autoapi.extension', 'numpydoc']
 autoapi_type = 'python'
 autoapi_dirs = ['../src']
+autoapi_options = ['members', 'undoc-members', 'show-inheritance', 'show-module-summary', 'imported-members']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
