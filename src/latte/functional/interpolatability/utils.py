677f4857d2c4d9c086c877e23708bcc79d07f7e7
Karn Watcharasupat <karn001@e.ntu.edu.sg>
2021-11-30 21:48:42 +0800

    add test for utils

--- src/latte/functional/interpolatability/utils.py
+++ src/latte/functional/interpolatability/utils.py
@@ -6,11 +6,9 @@ __VALID_MAX_MODE__ = ["naive", "lehmer"]
 __VALID_PTP_MODE__ = ["naive", "interdecile"]
 __VALID_REDUCE_MODE__ = ["all", "attribute", "sample", "none"]
 
+
 def _validate_za_shape(
-    z: np.ndarray,
-    a: np.ndarray,
-    reg_dim: Optional[List] = None,
-    min_size: int = None
+    z: np.ndarray, a: np.ndarray, reg_dim: Optional[List] = None, min_size: int = None
 ) -> Tuple[np.ndarray, np.ndarray, List]:
 
     assert 2 <= a.ndim <= 3
@@ -18,50 +16,54 @@ def _validate_za_shape(
 
     if a.ndim == 2:
         a = a[:, None, :]
-    
+
     if z.ndim == 2:
         z = z[:, None, :]
-        
+
     n_samples_a, n_attr, n_interp_a = a.shape
     n_samples_z, n_features, n_interp_z = z.shape
-    
+
     assert n_samples_a == n_samples_z
     assert n_interp_a == n_interp_z
-        
+    assert n_attr <= n_features
+
+    if min_size is not None:
+        assert n_interp_a >= min_size
+
     if reg_dim is not None:
-        n_attr = a.shape[-1]
-        n_features = z.shape[-1]
         assert len(reg_dim) == n_attr
         assert min(reg_dim) >= 0
         assert max(reg_dim) < n_features
-        
+
         z = z[:, reg_dim, :]
     else:
-        assert n_attr <= n_features
-        if n_attr != n_features:
+        if n_attr < n_features:
             z = z[:, :n_attr, :]
-        
-    if min_size is not None:
-        assert n_interp_a >= min_size
 
     return z, a
 
 
-def finite_diff(z: np.ndarray, a: np.ndarray, order: int = 1, mode: str = "forward", return_list: bool = False):
+def finite_diff(
+    z: np.ndarray,
+    a: np.ndarray,
+    order: int = 1,
+    mode: str = "forward",
+    return_list: bool = False,
+):
 
     rets = []
-    
-    for _ in range(order):
-        if mode == "forward":
+
+    if mode == "forward":
+        for _ in range(order):
             da = np.diff(a, n=1, axis=-1)
             dz = np.diff(z, n=1, axis=-1)
 
             a = da / dz
-            z = 0.5(z[..., :-1] + z[..., 1:])
-            
+            z = 0.5 * (z[..., :-1] + z[..., 1:])
+
             rets.append((a, z))
-        else:
-            raise NotImplementedError
+    else:
+        raise NotImplementedError
 
     if return_list:
         return rets
@@ -69,22 +71,30 @@ def finite_diff(z: np.ndarray, a: np.ndarray, order: int = 1, mode: str = "forwa
         return a, z
 
 
-def liad(z: np.ndarray, a: np.ndarray, order: int = 1, mode: str = "forward", return_list: bool = False):
+def liad(
+    z: np.ndarray,
+    a: np.ndarray,
+    order: int = 1,
+    mode: str = "forward",
+    return_list: bool = False,
+):
 
-    if mode in ["forward", "central"]:
+    if mode in ["forward"]:
         rets = finite_diff(z, a, order, mode, return_list=return_list)
     else:
-        #TODO: add spline interpolation derivatives
+        # TODO: add spline interpolation derivatives
         raise NotImplementedError
 
     return rets
 
+
 def lehmer_mean(x: np.ndarray, p: float):
-    
+
     if p == 1.0:
-        den = 1.0
+        den = np.ones_like(x)
     else:
-        den = np.power(x, p-1.0)
+        den = np.power(x, p - 1.0)
     num = x * den
-    
-    return np.sum(num, axis=-1)/np.sum(den, axis=-1)
+
+    return np.sum(num, axis=-1) / np.sum(den, axis=-1)
+
---
+++ tests/functional/interpolatability/test_utils.py
@@ -0,0 +1,175 @@
+from latte.functional.interpolatability import utils
+import numpy as np
+import pytest
+
+
+
+class TestShape:
+    def test_bad_samples(self):
+
+        with pytest.raises(AssertionError):
+            utils._validate_za_shape(np.random.randn(16, 32), np.random.randn(15, 32))
+            
+    def test_bad_interp(self):
+
+        with pytest.raises(AssertionError):
+            utils._validate_za_shape(np.random.randn(16, 1, 32), np.random.randn(16, 1, 2))
+            
+    def test_bad_features(self):
+
+        with pytest.raises(AssertionError):
+            utils._validate_za_shape(np.random.randn(16, 1, 32), np.random.randn(16, 3, 2))
+            
+    def test_bad_min_size(self):
+
+        with pytest.raises(AssertionError):
+            utils._validate_za_shape(np.random.randn(16, 1, 2), np.random.randn(16, 1, 2), min_size=3)
+            
+    def test_bad_regdim_shape(self):
+
+        with pytest.raises(AssertionError):
+            utils._validate_za_shape(np.random.randn(16, 8, 32), np.random.randn(16, 3, 32), reg_dim=[2, 0, 3, 4])
+            
+    def test_bad_regdim_neg(self):
+
+        with pytest.raises(AssertionError):
+            utils._validate_za_shape(np.random.randn(16, 8, 32), np.random.randn(16, 3, 32), reg_dim=[2, -1, 3])
+            
+    def test_bad_regdim_over(self):
+
+        with pytest.raises(AssertionError):
+            utils._validate_za_shape(np.random.randn(16, 8, 32), np.random.randn(16, 3, 32), reg_dim=[2, 8, 3])
+            
+    def test_regdim_slice(self):
+        zin = np.random.randn(16, 8, 32)
+        z, _ = utils._validate_za_shape(zin, np.random.randn(16, 3, 32), reg_dim=[3, 4, 5])
+        
+        np.testing.assert_equal(zin[:, [3, 4, 5], :], z)
+        
+    def test_regdim_auto(self):
+        zin = np.random.randn(16, 8, 32)
+        z, _ = utils._validate_za_shape(zin, np.random.randn(16, 3, 32), reg_dim=None)
+        
+        np.testing.assert_equal(zin[:, :3, :], z)
+        
+    def test_regdim_auto_eq(self):
+        zin = np.random.randn(16, 3, 32)
+        z, _ = utils._validate_za_shape(zin, np.random.randn(16, 3, 32))
+        
+        np.testing.assert_equal(zin, z)
+        
+    def test_auto_expand(self):
+        z, a = utils._validate_za_shape(np.random.randn(16, 32), np.random.randn(16, 32))
+        
+        assert z.shape == (16, 1, 32)
+        assert a.shape == (16, 1, 32)
+        
+    def test_bad_za_shapes(self):
+        with pytest.raises(AssertionError):
+            utils._validate_za_shape(np.random.randn(16,), np.random.randn(16, 32))
+            
+        with pytest.raises(AssertionError):
+            utils._validate_za_shape(np.random.randn(16,1,4,5), np.random.randn(16, 32))
+            
+        with pytest.raises(AssertionError):
+            utils._validate_za_shape(np.random.randn(16,1), np.random.randn(16, 32, 1, 3))
+            
+        with pytest.raises(AssertionError):
+            utils._validate_za_shape(np.random.randn(16,1), np.random.randn(16,))
+        
+
+class TestFiniteDiff:
+    def test_first_order(self):
+        z = np.stack([np.linspace(0.0, 1.0, 10) for _ in range(8)], axis=0)
+        a = np.stack([np.linspace(0.0, 0.5, 10) for _ in range(8)], axis=0)
+
+        dadz, zv = utils.finite_diff(z, a, order=1, mode="forward", return_list=False)
+
+        np.testing.assert_allclose(dadz, 0.5 * np.ones(shape=(8, 9)))
+        np.testing.assert_allclose(zv, 0.5 * (z[:, 1:] + z[:, :-1]))
+
+    def test_second_order(self):
+        z = np.stack([np.linspace(0.0, 1.0, 10) for _ in range(8)], axis=0)
+        a = np.stack([np.linspace(0.0, 0.5, 10) for _ in range(8)], axis=0)
+
+        dadz, zv = utils.finite_diff(z, a, order=2, mode="forward", return_list=False)
+
+        np.testing.assert_allclose(dadz, np.zeros(shape=(8, 8)))
+        z1 = 0.5 * (z[:, 1:] + z[:, :-1])
+        z2 = 0.5 * (z1[:, 1:] + z1[:, :-1])
+        np.testing.assert_allclose(zv, z2)
+
+    def test_list_return(self):
+        z = np.stack([np.linspace(0.0, 1.0, 10) for _ in range(8)], axis=0)
+        a = np.stack([np.linspace(0.0, 0.5, 10) for _ in range(8)], axis=0)
+
+        rets = utils.finite_diff(z, a, order=2, mode="forward", return_list=True)
+
+        assert len(rets) == 2
+
+        dadz, zv = rets[0]
+        d2adz2, z2v = rets[1]
+
+        np.testing.assert_allclose(dadz, 0.5 * np.ones(shape=(8, 9)))
+        np.testing.assert_allclose(zv, 0.5 * (z[:, 1:] + z[:, :-1]))
+        np.testing.assert_allclose(d2adz2, np.zeros(shape=(8, 8)))
+        z1 = 0.5 * (z[:, 1:] + z[:, :-1])
+        z2 = 0.5 * (z1[:, 1:] + z1[:, :-1])
+        np.testing.assert_allclose(z2v, z2)
+
+    def test_list_return3(self):
+        z = np.stack([np.linspace(0.0, 1.0, 10) for _ in range(8)], axis=0)
+        a = np.stack([np.linspace(0.0, 0.5, 10) for _ in range(8)], axis=0)
+
+        rets = utils.finite_diff(z, a, order=3, mode="forward", return_list=True)
+
+        assert len(rets) == 3
+
+    def test_mode(self):
+        z = np.stack([np.linspace(0.0, 1.0, 10) for _ in range(8)], axis=0)
+        a = np.stack([np.linspace(0.0, 0.5, 10) for _ in range(8)], axis=0)
+
+        with pytest.raises(NotImplementedError):
+            utils.finite_diff(z, a, order=1, mode="central", return_list=False)
+
+
+class TestLiad:
+    def test_liad_fd_agree(self):
+        z = np.random.rand(8, 16)
+        a = np.random.rand(8, 16)
+
+        d1, z1 = utils.finite_diff(z, a, order=1, mode="forward", return_list=False)
+        d2, z2 = utils.liad(z, a, order=1, mode="forward", return_list=False)
+
+        np.testing.assert_allclose(d1, d2)
+        np.testing.assert_allclose(z1, z2)
+
+    def test_liad2_fd_agree(self):
+        z = np.random.rand(8, 16)
+        a = np.random.rand(8, 16)
+
+        d1, z1 = utils.finite_diff(z, a, order=2, mode="forward", return_list=False)
+        d2, z2 = utils.liad(z, a, order=2, mode="forward", return_list=False)
+
+        np.testing.assert_allclose(d1, d2)
+        np.testing.assert_allclose(z1, z2)
+
+
+class TestLehmerMean:
+    def test_p1(self):
+        x = np.random.rand(8, 16)
+
+        m = utils.lehmer_mean(x, p=1.0)
+        np.testing.assert_allclose(m, np.mean(x, axis=-1))
+
+    def test_p2(self):
+        x = np.random.rand(8, 16)
+
+        m = utils.lehmer_mean(x, p=2.0)
+        np.testing.assert_allclose(m, np.sum(np.square(x), axis=-1) / np.sum(x, axis=-1))
+        
+    def test_p0(self):
+        x = np.random.rand(8, 16)
+
+        m = utils.lehmer_mean(x, p=0.0)
+        np.testing.assert_allclose(m, np.sum(np.ones_like(x), axis=-1) / np.sum(1.0/x, axis=-1))
