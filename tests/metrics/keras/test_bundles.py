e4482bba7d497dbe897c768884934eb36f5c0154
Karn Watcharasupat <karn001@e.ntu.edu.sg>
2021-12-16 22:12:30 +0800

    add torch bundle tests

--- tests/metrics/keras/test_bundles.py
+++ tests/metrics/keras/test_bundles.py
@@ -1,10 +1,7 @@
 try:
     import tensorflow as tf
 
-    from latte.metrics.keras.bundles import (
-        DependencyAwareMutualInformationBundle,
-        LiadInterpolatabilityBundle,
-    )
+    from latte.metrics.keras.bundles import DependencyAwareMutualInformationBundle
     from latte.metrics.keras.disentanglement import (
         MutualInformationGap,
         DependencyAwareMutualInformationGap,
@@ -24,45 +21,28 @@ import pytest
 class TestDMIBundle:
     def test_dmi_bundle(self):
 
-        for reg_dim in [None, [3, 7, 4]]:
-            for discrete in [True, False]:
+        bundle = DependencyAwareMutualInformationBundle()
+        mig = MutualInformationGap(fill_reg_dim=True)
+        dmig = DependencyAwareMutualInformationGap()
+        dlig = DependencyAwareLatentInformationGap()
+        xmig = DependencyBlindMutualInformationGap()
 
-                bundle = DependencyAwareMutualInformationBundle(
-                    reg_dim=reg_dim, discrete=discrete
-                )
-                mig = MutualInformationGap(
-                    reg_dim=reg_dim, discrete=discrete, fill_reg_dim=True
-                )
-                dmig = DependencyAwareMutualInformationGap(
-                    reg_dim=reg_dim, discrete=discrete
-                )
-                dlig = DependencyAwareLatentInformationGap(
-                    reg_dim=reg_dim, discrete=discrete
-                )
-                xmig = DependencyBlindMutualInformationGap(
-                    reg_dim=reg_dim, discrete=discrete
-                )
+        z = tf.random.uniform(shape=(16, 16))
+        a = tf.random.uniform(shape=(16, 3))
 
-                for _ in range(3):
-                    z = tf.random.uniform(shape=(16, 16))
-                    a = tf.random.uniform(shape=(16, 3))
+        bundle.update_state(z=z, a=a)
+        mig.update_state(z, a)
+        dmig.update_state(z, a)
+        dlig.update_state(z, a)
+        xmig.update_state(z, a)
 
-                    if discrete:
-                        a = tf.cast(tf.round(10 * a), tf.int32)
+        bundle_out = bundle.result()
+        indiv_out = {
+            "MIG": mig.result(),
+            "DMIG": dmig.result(),
+            "DLIG": dlig.result(),
+            "XMIG": xmig.result(),
+        }
 
-                    bundle.update_state(z=z, a=a)
-                    mig.update_state(z, a)
-                    dmig.update_state(z, a)
-                    dlig.update_state(z, a)
-                    xmig.update_state(z, a)
-
-                bundle_out = bundle.result()
-                indiv_out = {
-                    "MIG": mig.result(),
-                    "DMIG": dmig.result(),
-                    "DLIG": dlig.result(),
-                    "XMIG": xmig.result(),
-                }
-
-                for key in ["MIG", "DMIG", "DLIG", "XMIG"]:
-                    tf.assert_equal(bundle_out[key], indiv_out[key])
+        for key in ["MIG", "DMIG", "DLIG", "XMIG"]:
+            tf.assert_equal(bundle_out[key], indiv_out[key])
